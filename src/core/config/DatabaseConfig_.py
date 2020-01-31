257a8790974b940d3e05f6d162561f99160ea0a1
Alvinditya Saputra <alvindityas@gmail.com>
2020-01-31 15:33:37 +0700

    Change Database Config

--- src/core/config/DatabaseConfig_.py
+++ src/core/config/DatabaseConfig_.py
@@ -1,11 +1,11 @@
 class DatabaseConfig:
 
     def __init__(self):
-        self.DB_USERNAME = 'root'
-        self.DB_PASSWORD = 'Maverick782'
+        self.DB_USERNAME = '<DATABASE_USERNAME>'
+        self.DB_PASSWORD = '<DATABASE_PASSWORD>'
         self.DB_HOST = 'localhost'
         self.DB_PORT = 3306
-        self.DB_NAME = 'my_flask_project'
+        self.DB_NAME = '<DATABASE_NAME>'
 
     def get_connection(self):
         return f'mysql+mysqlconnector://{self.DB_USERNAME}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}?use_pure=True'
